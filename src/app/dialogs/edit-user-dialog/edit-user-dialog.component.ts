c842f0102bebb98681f8d6f4ad2cff24440ba6c2
JJLorente <joel.jimenez.lorente@gmail.com>
2023-05-22 19:54:40 +0200

    templates

--- src/app/dialogs/edit-user-dialog/edit-user-dialog.component.ts
+++ src/app/dialogs/edit-user-dialog/edit-user-dialog.component.ts
@@ -5,26 +5,7 @@ import { UsersService } from '../../services/employees.service';
 @Component({
   selector: 'app-edit-user-dialog',
   styleUrls: ['./edit-user-dialog.component.css'],
-  template: `
-    <ng-container *ngIf="user">
-      <h2 mat-dialog-title class="dialog-title">User edit</h2>
-      <div class="form-container">
-        <mat-form-field class="form-field">
-          <input matInput placeholder="Name" [(ngModel)]="user.name">
-        </mat-form-field>
-        <mat-form-field class="form-field">
-          <input matInput placeholder="Phone" [(ngModel)]="user.phone">
-        </mat-form-field>
-        <mat-form-field class="form-field">
-          <input matInput placeholder="Email" [(ngModel)]="user.mail">
-        </mat-form-field>
-      </div>
-      <div mat-dialog-actions class="actions-div">
-        <button mat-flat-button mat-dialog-close color="default" class="cancel-btn">Cancel</button>
-        <button mat-flat-button mat-dialog-close color="primary" class="edit-btn" (click)="saveChanges()">Save</button>
-      </div>
-    </ng-container>
-  `,
+  templateUrl: './edit-user-dialog.component.html',
 })
 export class EditUserDialogComponent {
   userId: number;
